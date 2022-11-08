c55aaf66a688d7809e5de12abf88a86e91f95d90
gbloch <gaetan.bloch@gmail.com>
2022-11-08 19:21:16 +0100

    feat: add galaxy db reader in odds service

--- core/src/main/java/io/gbloch/falcon/challenge/core/application/service/OddsService.java
+++ core/src/main/java/io/gbloch/falcon/challenge/core/application/service/OddsService.java
@@ -1,7 +1,10 @@
 package io.gbloch.falcon.challenge.core.application.service;
 
+import com.google.common.graph.MutableValueGraph;
 import io.gbloch.falcon.challenge.core.application.FalconCoreException;
 import io.gbloch.falcon.challenge.core.application.input.port.ComputeOddsUseCase;
+import io.gbloch.falcon.challenge.core.application.service.db.GalaxyDbException;
+import io.gbloch.falcon.challenge.core.application.service.db.GalaxyDbFileReader;
 import io.gbloch.falcon.challenge.core.application.service.parser.FalconFileException;
 import io.gbloch.falcon.challenge.core.application.service.parser.FalconFileParser;
 import io.gbloch.falcon.challenge.core.domain.Empire;
@@ -12,14 +15,16 @@ import lombok.RequiredArgsConstructor;
 public final class OddsService implements ComputeOddsUseCase {
 
     final FalconFileParser falconFileParser;
-
+    final GalaxyDbFileReader galaxyDbFileReader;
     FalconConfig falconConfig;
+    MutableValueGraph<String, Integer> galaxy;
 
     @Override
     public int whatAreTheOdds(String configFilePath, Empire empire) throws FalconCoreException {
         try {
             falconConfig = falconFileParser.parseFile(configFilePath);
-        } catch (FalconFileException e) {
+            galaxy = galaxyDbFileReader.readFile(falconConfig.routesDbPath());
+        } catch (FalconFileException | GalaxyDbException e) {
             throw new FalconCoreException(e);
         }
         return -1;
--- core/src/test/java/io/gbloch/falcon/challenge/core/application/input/port/ComputeOddsUseCaseTest.java
+++ core/src/test/java/io/gbloch/falcon/challenge/core/application/input/port/ComputeOddsUseCaseTest.java
@@ -1,19 +1,29 @@
 package io.gbloch.falcon.challenge.core.application.input.port;
 
 import static io.gbloch.falcon.challenge.core.application.TestUtils.CONFIG_FILE_PATH;
+import static io.gbloch.falcon.challenge.core.application.TestUtils.DAGOBAH;
+import static io.gbloch.falcon.challenge.core.application.TestUtils.ENDOR;
 import static io.gbloch.falcon.challenge.core.application.TestUtils.HOTH;
+import static io.gbloch.falcon.challenge.core.application.TestUtils.TATOOINE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchException;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.common.collect.RowSortedTable;
+import com.google.common.collect.TreeBasedTable;
+import com.google.common.graph.MutableValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
 import io.gbloch.falcon.challenge.core.application.FalconCoreException;
 import io.gbloch.falcon.challenge.core.application.service.OddsService;
+import io.gbloch.falcon.challenge.core.application.service.db.GalaxyDbException;
+import io.gbloch.falcon.challenge.core.application.service.db.GalaxyDbFileReader;
 import io.gbloch.falcon.challenge.core.application.service.parser.FalconFileException;
 import io.gbloch.falcon.challenge.core.application.service.parser.FalconFileParser;
 import io.gbloch.falcon.challenge.core.domain.BountyHunter;
 import io.gbloch.falcon.challenge.core.domain.Empire;
+import io.gbloch.falcon.challenge.core.domain.FalconConfig;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,6 +38,10 @@ class ComputeOddsUseCaseTest {
 
     @Mock
     private FalconFileParser falconFileParser;
+    @Mock
+    private GalaxyDbFileReader dbFileReader;
+
+    private FalconConfig falconConfig;
 
     private static Empire createEmpire(int countdown) {
         return new Empire(countdown,
@@ -38,6 +52,7 @@ class ComputeOddsUseCaseTest {
 
     @BeforeEach
     void setUp() {
+        this.falconConfig = new FalconConfig(6, TATOOINE, ENDOR, "universe.db");
     }
 
     @ParameterizedTest
@@ -45,7 +60,7 @@ class ComputeOddsUseCaseTest {
     void when_computeOddsWithCountdown_then_getExcpectedOdds(int countdown, int oddsExcepted)
         throws FalconCoreException {
         // GIVEN
-        ComputeOddsUseCase oddsUseCase = new OddsService(falconFileParser);
+        ComputeOddsUseCase oddsUseCase = new OddsService(falconFileParser, dbFileReader);
         Empire empire = createEmpire(countdown);
 
         // WHEN
@@ -60,7 +75,25 @@ class ComputeOddsUseCaseTest {
         // GIVEN
         when(falconFileParser.parseFile(anyString())).thenThrow(mock(FalconFileException.class));
         Empire empire = createEmpire(0);
-        ComputeOddsUseCase oddsUseCase = new OddsService(falconFileParser);
+        ComputeOddsUseCase oddsUseCase = new OddsService(falconFileParser, dbFileReader);
+
+        // WHEN
+        Exception exception = catchException(
+            () -> oddsUseCase.whatAreTheOdds(CONFIG_FILE_PATH, empire)
+        );
+
+        // THEN
+        assertThat(exception).isExactlyInstanceOf(FalconCoreException.class);
+    }
+
+    @Test
+    void given_galaxyDbError_when_computeOdds_then_getException()
+        throws GalaxyDbException, FalconFileException {
+        // GIVEN
+        when(dbFileReader.readFile(anyString())).thenThrow(GalaxyDbException.class);
+        when(falconFileParser.parseFile(anyString())).thenReturn(falconConfig);
+        Empire empire = createEmpire(0);
+        ComputeOddsUseCase oddsUseCase = new OddsService(falconFileParser, dbFileReader);
 
         // WHEN
         Exception exception = catchException(
