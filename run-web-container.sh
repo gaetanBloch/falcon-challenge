d5a48e256672a85d212b87817332981b6c45ebf6
Gaëtan Bloch <gaetan.bloch@gmail.com>
2022-11-15 11:34:47 +0100

    chore: update run-web-container.sh

--- run-web-container.sh
+++ run-web-container.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mvn clean package -pl web -am
-cd web && docker build -f src/main/docker/Dockerfile.uber-jar -t quarkus/falconchallenge-uber-jvm .
+./mvnw clean package -pl web -am
+cd web
+docker build -f src/main/docker/Dockerfile.uber-jar -t quarkus/falconchallenge-uber-jvm .
 docker run -i --rm -p 8080:8080 quarkus/falconchallenge-uber-jvm
